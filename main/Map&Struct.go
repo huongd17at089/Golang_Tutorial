f2c3d1532c6c85383831bd573936e9154f65930d
dinhgihuong <huongdtt.b17at089@gmail.com>
2021-08-01 16:50:52 +0700

    day2 week1: costant map struct array

---
+++ main/Map&Struct.go
@@ -0,0 +1,99 @@
+package main
+
+import "fmt"
+
+/*
+summary
+map
+- key : value
+- make()
+- check exist by ok
+- key same type, val same type
+- copy same underlying data
+
+struct
+- colect any type
+- field
+- normal type and anonymous type
+- variable type
+- no inheritance
+- can embeding
+- tags
+ */
+
+type Persion struct {
+	Name string //'required max: "10"'
+	Age int
+	Abc []int
+}
+
+// embeding
+
+type Doctor struct {
+	Persion //composition relationship = has a
+	Hospital string
+}
+func main(){
+//map
+	//create
+	m := map[string]int{
+		"hanoi" : 1,
+		"vn" : 2,
+	}
+	// m := map[[]int]string{} error
+	mep := map[[3]int]string{}
+	fmt.Println(m, mep)
+	fmt.Println(m["hanoi"]) // 1
+	m["HCM"] = 3 // add key
+	pop, ok := m["hanoi"]
+	fmt.Println(pop, ok) // 1, true = key in map
+	//buil in
+	delete(m, "HCM") // del key
+	fmt.Println(len(m))
+	makemap := make(map[string]int)
+	makemap = m
+	fmt.Println(makemap)
+	//maniputation
+	a := map[string]int{
+		"hanoi" : 1,
+		"vn" : 2,
+	}
+	b := a
+	delete(b, "vn")
+	fmt.Println(b) // hanoi :1
+	fmt.Println(a) // hanoi : 1
+
+//struct
+	//create
+	persion := Persion{
+		Name: "hihi",
+		Age: 22,
+		Abc: []int{1,2,3},
+	}
+	fmt.Print(persion)
+	fmt.Println(persion.Age)
+
+	doctor := struct {
+		name string
+	}{name : "hu"}
+	fmt.Println(doctor)
+	other := doctor
+	other.name = "hi"
+	fmt.Println(doctor.name) // hu
+	fmt.Println(other.name) //hi
+
+	otherx := &doctor
+	fmt.Println(otherx)
+	//naming
+
+	//embeding
+	d := Doctor{}
+	d.Name = "huhi"
+	d.Hospital = "lala"
+	fmt.Println(d.Name)
+	//tags
+/*
+
+ */
+
+}
--- main/constants.go
+++ main/constants.go
@@ -1,5 +1,58 @@
 package main
 
-func main(){
+import (
+	"fmt"
+)
 
+const (
+	a = iota // 0
+	b // 1
+	c // 2
+)
+
+const (
+	a2 = iota // 0
+)
+
+const (
+	_ = iota + 5 // 0 +5
+	// 1 << ( 10 * iota)
+	h // 6 = 1+ 5
+	k // 7
+
+)
+
+func main(){
+	/* maming convention
+		PascalCase for exported constants
+		camel -> internal
+	 */
+// type : interoparate with same type
+	const MYCONST int = 123
+	//const MyConst float64 = math.Abs(5)
+	//MyConst = 100 error
+//untype : interoparate with similar type
+	const (
+		PI = 3.14
+		HELLOWORLD= "hello world!"
+	)
+	const x = 12
+	var y int16 = 13
+	fmt.Println(x+y)
+	fmt.Println(PI)
+//enumerated
+/*
+- start at 0
+ */
+	fmt.Println(a,b,c, a2) // 0 1 2 0
+
+//Enumeration expressions : arithmetic, bitswise bitshifting
+
+/* summary
+   - all primitive type : int float string boolen
+   	- can shadow
+   	- imutable
+   	- operation +  %- * /
+
+*/
 }
